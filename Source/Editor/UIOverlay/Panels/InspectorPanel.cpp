53ea9532e6945058dec05ac1d426fdd66c163bf8
Filippo-BSW <filippobsw@gmail.com>
2022-06-13 16:07:56 +0900

    Commit

--- Source/Editor/UIOverlay/Panels/InspectorPanel.cpp
+++ Source/Editor/UIOverlay/Panels/InspectorPanel.cpp
@@ -253,7 +253,7 @@ namespace adh {
                     std::string cmd2 = "open " + meshBuffer->meshFilePath;
                     system(cmd2.data());
 #elif defined(ADH_LINUX)
-                    std::string cmd2 = "vim " + meshBuffer->meshFilePath + " &";
+                    std::string cmd2 = "xdg-open " + meshBuffer->meshFilePath + " &";
                     system(cmd2.data());
 #elif defined(ADH_WINDOWS)
                     std::string cmd2 = "start " + meshBuffer->meshFilePath;
@@ -402,7 +402,7 @@ namespace adh {
                     std::string cmd2 = "open " + component.filePath;
                     system(cmd2.data());
 #elif defined(ADH_LINUX)
-                    std::string cmd2 = "vim " + component.filePath + " &";
+                    std::string cmd2 = "xdg-open " + component.filePath + " &";
                     system(cmd2.data());
 #elif defined(ADH_WINDOWS)
                     std::string cmd2 = "start " + component.filePath;
@@ -614,7 +614,7 @@ namespace adh {
                     auto scriptDir   = vk::Context::Get()->GetDataDirectory();
                     std::string cmd1 = "cp " + scriptDir + "Resources/Scripts/template.lua " +
                                        scriptDir + "Assets/Scripts/" + buf + ".lua";
-                    std::string cmd2 = "vim " + scriptDir + "Assets/Scripts/" + buf + ".lua &";
+                    std::string cmd2 = "xdg-open" + scriptDir + "Assets/Scripts/" + buf + ".lua &";
                     system(cmd1.data());
                     system(cmd2.data());
 #elif defined(ADH_WINDOWS)
