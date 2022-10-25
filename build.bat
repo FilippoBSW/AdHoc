4987599f33cc32945fe30157a6b6930f3ca913b1
Filippo-BSW <filippobsw@gmail.com>
2022-10-25 19:13:02 +0900

    Commit

--- build.bat
+++ build.bat
@@ -28,9 +28,15 @@ if not exist "build\NUL" mkdir "build"
 cd build
 
 cmake -DUSE_PLATFORM="Windows"^
+	  -DCMAKE_BUILD_TYPE=%*^
 	  -DASSIMP_BUILD_TESTS=OFF^
 	  -DASSIMP_BUILD_ASSIMP_TOOLS=OFF^
 	  -DBUILD_TESTING=OFF^
+        -DALSOFT_EXAMPLES=OFF^
+        -DALSOFT_INSTALL_EXAMPLES=OFF^
+        -DSDL2MAIN_LIBRARY=""^
+        -DSDL2_CORE_LIBRARY=""^
+        -DSDL2_INCLUDE_DIR=""^
 	  -DYAML_CPP_BUILD_TOOLS=OFF ..
 cmake --build . --config %*
 
